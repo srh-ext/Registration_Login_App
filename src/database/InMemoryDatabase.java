350c7bffeab5ea71b0c4ba760f96766e878c6b83
Yuriy S <dev@solovyov.de>
2023-08-28 15:29:36 +0200

    Database class and add user function

--- src/database/InMemoryDatabase.java
+++ src/database/InMemoryDatabase.java
@@ -1,4 +1,27 @@
 package database;
 
+import dao.User;
+
+import java.util.ArrayList;
+
 public class InMemoryDatabase {
+
+    private ArrayList<User> userDatabase;
+
+    public InMemoryDatabase() {
+        this.userDatabase = new ArrayList<>();
+    }
+
+    /**
+     * Adds new user to the in memory database. If not empty.
+     * @param user User object
+     * @return boolean if user successfully added, false if null or not added.
+     */
+    public boolean addUser(User user) {
+        if (user != null) {
+            //TODO: check if user already exists
+            return this.userDatabase.add(user);
+        }
+        return false;
+    }
 }
