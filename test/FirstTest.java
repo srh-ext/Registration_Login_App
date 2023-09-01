c278bb4256f721f07a346eab4b1f35c32b827e77
Yuriy S <dev@solovyov.de>
2023-09-01 10:30:13 +0200

    first junit tests

---
+++ test/FirstTest.java
@@ -0,0 +1,28 @@
+import dao.User;
+import database.InMemoryDatabase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class FirstTest {
+    @Test
+    public void testIfInitialDBIsEmpty() {
+        System.out.println("testIfInitialDBIsEmpty");
+        InMemoryDatabase db = new InMemoryDatabase();
+        int size = db.getAllUser().size();
+        Assert.assertTrue(size == 0);
+    }
+
+    @Test
+    public void testIfDBContainsOneUser() {
+        System.out.println("testIfDBContainsOneUser");
+        InMemoryDatabase db = new InMemoryDatabase();
+        int size = db.getAllUser().size();
+        Assert.assertTrue(size == 0);
+
+        User user1 = new User();
+        db.addUser(user1);
+        int size2 = db.getAllUser().size();
+        Assert.assertTrue(size2 == 1);
+    }
+}
