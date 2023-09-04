44408cced6d5eda9c114dc54deb58d955fc3a8ad
Yuriy S <dev@solovyov.de>
2023-09-04 15:04:51 +0200

    add structure for UserManagementTest

---
+++ test/UserManagementTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class UserManagementTest {
+
+    @Test
+    public void testSaveUser() {
+        System.out.println("testSaveUser");
+    }
+
+    @Test
+    public void testAuthenticateUser() {
+        System.out.println("testAuthenticateUser");
+    }
+}
