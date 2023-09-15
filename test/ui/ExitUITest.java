fb0ed157b45f21e3ab728fb400437edb8844e8f0
Yuriy S <dev@solovyov.de>
2023-09-15 10:20:13 +0200

    moved tests

--- test/ui/ExitUITest.java
+++ test/ui/ExitUITest.java
@@ -3,31 +3,5 @@ package ui;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ExitUITest {
-
-    @Test
-    public void testSetMenuName() {
-        System.out.println("testSetMenuName");
-        UIMenu exitMenu = new ExitUI();
-        Assert.assertEquals(exitMenu.getMenuName(), "Exit");
-        exitMenu.setMenuName("Test");
-        Assert.assertEquals(exitMenu.getMenuName(), "Test");
-    }
-
-    @Test
-    public void testGetMenuName() {
-        System.out.println("testGetMenuName");
-        UIMenuInterface exitMenu = new ExitUI();
-        System.out.println(exitMenu.getClass().toString());
-        Assert.assertTrue(exitMenu instanceof ExitUI);
-        Assert.assertNotNull(exitMenu.getMenuName());
-        Assert.assertEquals(exitMenu.getMenuName(), "Exit");
-    }
-
-    @Test
-    public void testIsExit() {
-        System.out.println("testIsExit");
-        UIMenuInterface exitMenu = new ExitUI();
-        Assert.assertTrue(exitMenu.isExit());
-    }
+public class ExitUITest extends UITest {
 }
--- test/ui/UITest.java
+++ test/ui/UITest.java
@@ -1,4 +1,33 @@
 package ui;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public abstract class UITest {
+
+    @Test
+    public final void testSetMenuName() {
+        System.out.println("testSetMenuName");
+        UIMenu exitMenu = new ExitUI();
+        Assert.assertEquals(exitMenu.getMenuName(), "Exit");
+        exitMenu.setMenuName("Test");
+        Assert.assertEquals(exitMenu.getMenuName(), "Test");
+    }
+
+    @Test
+    public final void testGetMenuName() {
+        System.out.println("testGetMenuName");
+        UIMenuInterface exitMenu = new ExitUI();
+        System.out.println(exitMenu.getClass().toString());
+        Assert.assertTrue(exitMenu instanceof ExitUI);
+        Assert.assertNotNull(exitMenu.getMenuName());
+        Assert.assertEquals(exitMenu.getMenuName(), "Exit");
+    }
+
+    @Test
+    public final void testIsExit() {
+        System.out.println("testIsExit");
+        UIMenuInterface exitMenu = new ExitUI();
+        Assert.assertTrue(exitMenu.isExit());
+    }
 }
